b9e3cdb1849a02ccda002a07b380695bc8898655
Jeselyn Tania <jeselyntan01@gmail.com>
2024-12-05 13:38:51 +0700

    first

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid" style="background-color: #F4F7F0;">
+            <img src="{{asset('assets/logo.png')}}" alt="" width="200" height="75">
+            
+            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active rounded-5 text-center me-4" style="background-color: #E7E7E7; width: 100px" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active rounded-5 text-center me-4" style="background-color: #E7E7E7; width: 100px" aria-current="page" href="#">Drop In</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active rounded-5 text-center me-4" style="background-color: #E7E7E7; width: 100px" aria-current="page" href="#">Mission</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active rounded-5 text-center me-4" style="background-color: #E7E7E7; width: 100px" aria-current="page" href="#">About Us</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <img src="{{asset('icons/profileicon.svg')}}" alt="" width="25" height="25">
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <div class="position-absolute">
+            <img src="{{asset('assets/banner.jpeg')}}" alt="" width="100%" height="100%">
+            <div class="d-flex flex-column justify-content-center align-items-center position-absolute top-50 start-50 translate-middle">
+                <p class="fs-1 fw-bold" style="color: #183F23;">Dispose Waste, Earn Rewards!</p>
+                <p class="text-center" style="width: 475px;">Turn your waste into rewards with SmartWaste. Dispose responsibly, complete missions, and redeem points for exclusive rewards!</p>
+                <button class="px-5 py-2 rounded-3 border border-0" style="background-color: #183F23; color: white;">Join SmartWaste Today!</button>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <p>Welcome to SmartWaste!</p>
+                <p>Your Smart Way to Dispose and Earn!</p>
+                <p>At SmartWaste, we make it easy to dispose of your waste responsibly while earning rewards. Whether it’s organic or non-organic waste, you can drop it off at our warehouse, complete missions to earn extra points, and redeem those points for exclusive vouchers. Join us in creating a cleaner, greener future—one smart drop at a time!</p>
+            </div>
+
+            <div>
+                <p>How SmartWaste Works</p>
+
+                <div class="position-absolute" style="background-color: #F4F7F0;">
+                    <img class="top-0 start-50" src="{{asset('assets/dropin.png')}}" alt=""/>
+
+                    <p>Drop In</p>
+                    <p>Dispose of your organic and non-organic waste at our SmartWaste warehouse by filling out a simple form. Every drop-off brings us closer to a cleaner environment.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
