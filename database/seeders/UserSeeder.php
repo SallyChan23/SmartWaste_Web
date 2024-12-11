93d3b087f0626278930d079320326a28ecd1e040
Jeselyn Tania <jeselyntan01@gmail.com>
2024-12-11 10:27:41 +0700

    push

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,8 +13,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        
-
         User::create([
             'username'=>'Admin',
             'email'=>'admin@gmail.com',
