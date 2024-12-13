c8260ed95e95dc10d20a374b609e97924e1e823a
aidahnf <aiidaahf@gmail.com>
2024-12-13 19:10:56 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MissionController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AboutUsController;
