29a359589eef40dd79143a6949203b04260d9664
Feren Lisady <ferenlisady06@gmail.com>
2024-12-14 22:15:32 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -26,12 +26,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 Route::middleware(['web'])->group(function () {
@@ -46,17 +42,11 @@ Route::middleware(['web'])->group(function () {
 
 });
 
-
-
 Route::middleware(['auth', 'CheckRole:user,admin'])->group(function () {
     Route::resource('mission', MissionController::class);
     Route::resource('voucher',VoucherController::class)->middleware(['auth','CheckRole:user,admin']);
     
 });
-
-Route::post('/mission/start/{missionId}', [MissionController::class, 'startMission'])->name('mission.start');
-Route::put('/mission/update-progress/{missionTransactionId}', [MissionController::class, 'updateProgress'])->name('mission.updateProgress');
-Route::post('/voucher/redeem/{voucherId}', [VoucherController::class, 'redeem'])->name('voucher.redeem');
 
 // Routes for users
 Route::middleware(['auth', 'CheckRole:user'])->group(function () {
@@ -68,32 +58,16 @@ Route::middleware(['auth', 'CheckRole:user'])->group(function () {
     Route::get('/drop_in', [DropInController::class, 'index'])->name('drop_in.index');
 });
 
-Route::middleware(['web'])->group(function () {
-    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('auth.login');
-    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-    Route::post('/profile/update-picture', [ProfileController::class, 'updatePicture'])->name('profile.updatePicture');
-    Route::get('/report', [ReportController::class, 'index'])->name('report');
-    Route::get('/redeem', [RedeemController::class, 'index'])->name('redeem');
-
-});
-
-
 // Routes for admins
 Route::middleware(['auth', 'CheckRole:admin'])->group(function () {
-   
     Route::get('/admin/drop_in', [AdminController::class, 'index'])->name('admin.drop_in.index');
     Route::get('/admin/drop_in/{dropIn}/review', [AdminController::class, 'review'])->name('admin.drop_in.review');
     Route::post('/admin/drop_in/{dropIn}/update', [AdminController::class, 'update'])->name('admin.drop_in.update');
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/set-locale/{locale}', function($locale) {
-        if(in_array($locale, ['en', 'id'])){
-            session(['locale' =>$locale]);
-        }
-        return redirect()->back();
-    })->name('set-locale');
-});
+Route::get('/set-locale/{locale}', function($locale) {
+    if(in_array($locale, ['en', 'id'])){
+        session(['locale' =>$locale]);
+    }
+    return redirect()->back();
+})->name('set-locale');
