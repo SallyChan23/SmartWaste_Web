c4d40a67fd2647f6ab5719b33107687e5762d52f
Feren Lisady <ferenlisady06@gmail.com>
2024-12-15 23:40:34 +0700

    lang footer

---
+++ resources/lang/id/footer.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'company'=>'Perusahaan',
+    'about-us'=>'Tentang Kami',
+    'service'=>'Layanan Kami',
+    'contact'=>'Hubungi Kami',
+    'help'=>'Dapatkan Bantuan',
+    'faq'=>'SSD',
+    'shipping'=>'Pengiriman',
+    'drop-in'=>'Status Drop In',
+    'socials'=>'Sosial'
+];
