271e066094b9a000c048fa685b17648179fd3bdf
aidahnf <aiidaahf@gmail.com>
2024-12-15 15:57:49 +0700

    Update RedeemController.php

--- app/Http/Controllers/RedeemController.php
+++ app/Http/Controllers/RedeemController.php
@@ -1,21 +1,28 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\VoucherTransaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Voucher;
+
 class RedeemController extends Controller
 {
-    //
     public function index()
     {
-        // Logic for the report page
-        $redeemedVouchers = VoucherTransaction::where('userId', Auth::id())
-        ->with('voucher') // Assuming the relationship is defined
-        ->get();
+        // Check if the user is authenticated
+        if (!Auth::check()) {
+            return redirect()->route('auth.login')->withErrors('Please log in to view your profile.');
+        }
 
-        return view('profile.redeem', compact('redeemedVouchers'));
+        $user = Auth::user();
+
+        // Logic for retrieving redeemed vouchers
+        $redeemedVouchers = VoucherTransaction::where('userId', Auth::id())
+            ->with('voucher') // Assuming the relationship is defined
+            ->get();
+
+        // Return the appropriate view
+        return view('profile.redeem', compact('redeemedVouchers', 'user'));
     }
 }
+
