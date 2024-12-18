43d038ae745f72c93f030a4b58ca5e12fdc57649
Feren Lisady <ferenlisady06@gmail.com>
2024-12-19 00:26:19 +0700

    localization navbar

---
+++ resources/lang/en/navbar.php
@@ -0,0 +1,11 @@
+<?php
+
+return[
+    'menu' => 'Menu',
+    'home' => 'Home',
+    'dropin' => 'Drop In',
+    'admindi' => 'Admin Drop In',
+    'mission' => 'Mission',
+    'voucher' => 'Voucher',
+    'about' => 'About Us',
+];
---
+++ resources/lang/id/navbar.php
@@ -0,0 +1,11 @@
+<?php
+
+return[
+    'menu' => 'Menu',
+    'home' => 'Beranda',
+    'dropin' => 'Drop In',
+    'admindi' => 'Admin Drop In',
+    'mission' => 'Misi',
+    'voucher' => 'Voucher',
+    'about' => 'Tentang Kami',
+];
